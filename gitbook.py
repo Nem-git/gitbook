e4dc428fd3ac3a553fadcd1c720e73b8c0c8d0e7
Nem-git <loukastjean06@gmail.com>
2024-05-18 15:07:48 -0400

    Added typing and download to specific folder

--- gitbook.py
+++ gitbook.py
@@ -1,3 +1,4 @@
+import os
 import asyncio
 import aiohttp
 import aiofiles
@@ -6,31 +7,50 @@ from bs4 import BeautifulSoup
 
 
 
-async def Download(path, url) -> None:
-    
+async def download(path : str, url : str) -> str:
+    name = f"{url.split("/")[-1]}.html"
     async with aiohttp.ClientSession() as session:
         async with session.get(url=url) as resp:
             if resp.ok:
-                async with aiofiles.open(file=path, mode="wb") as file:
-                    async with chunk in resp.content.iter_chunked(n=8192):
+                async with aiofiles.open(file=f"{path}/{name}", mode="wb") as file:
+                    async for chunk in resp.content.iter_chunked(n=8192):
                         await file.write(chunk)
+                    return name
+
             else:
                 print(f"Server gave back error {resp.status}")
 
 
-async def Rip(path) -> None:
-    async with aiofiles.open(file=path, mode="r") as file:
-        soup = BeautifulSoup(file, "html.parser")
+async def rip(dir_path : str, url, url_root : str, url_start : str) -> None:
 
+    os.makedirs(f"{dir_path}{url_root}", exist_ok=True)
+    dir_path = f"{dir_path}{url_root}"
+    file_name = await download(path=dir_path, url=url)
+
+    async with aiofiles.open(file=f"{dir_path}/{file_name}", mode="rt", newline="\n") as file:
+        soup = BeautifulSoup(await file.read(), "html.parser")
+        for link in soup.find_all("a"):
+            async with asyncio.TaskGroup() as tg:
+                tg.create_task(coro=task_management(url_root, dir_path, url_start, link))
+
+
+async def task_management(url_root : str, dir_path : str, url_start : str, link : str):
+    l = link.get("href")
+    if url_root in l:
+        await download(dir_path, f"{url_start}{l}")
+        print(l)
 
 
 if __name__ == "__main__":
-    base_url:str = "https://gitbook.io/"
     
-    path = "../stuff.html"
-    url = "https://groupeinfo.gitbook.io/computer-science-data-base"
+    base_url:str = "https://gitbook.io"
+    url_start = "https://groupeinfo.gitbook.io"
+    dir_path = ".."
+    url_root = "/computer-science-data-base"
+    url = "".join((url_start, url_root))
     
-    asyncio.run(main=Download(path=path, url=url))
+    
+    asyncio.run(main=rip(dir_path=dir_path, url=url, url_root=url_root, url_start=url_start))
     
     
     
