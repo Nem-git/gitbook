541874feceda6b3f46257c894042e8f797b7f893
Nem-git <loukastjean06@gmail.com>
2024-05-19 17:39:19 -0400

    Rewritten all the link class and made improvements to speed and clarity of code

--- gitbook.py
+++ gitbook.py
@@ -57,101 +57,61 @@ class Time:
 
 class Link:
 
-    async def download(self, path : str, url : str) -> str:
-        name = url.split("/")[-1] + ".html"
+    async def download(self, path : str, url : str) -> None:
         async with aiohttp.ClientSession() as session:
-            print(url)
 
             async with session.get(url=url) as resp:
                 if resp.ok:
-                    async with aiofiles.open(file=f"{path}/{name}", mode="wb") as file:
-                        async for chunk in resp.content.iter_chunked(n=8192):
+                    async with aiofiles.open(file=path, mode="wb") as file:
+                        async for chunk in resp.content.iter_chunked(n=41920):
                             await file.write(chunk)
-                        return name
 
                 else:
                     print(f"Server gave back error {resp.status}")
 
 
-    async def rip(self, dir_path : str, url, url_root : str, url_start : str, link : str) -> None:
+    async def management(self, path, url, dir_path) -> None:
+        await self.download(path, url)
         
-        file_name = await self.management(url_root, dir_path, url_start, link)
-        dir_path = f"{dir_path}{url_root}"
-
-        async with aiofiles.open(file=f"{dir_path}/{file_name}", mode="rt", newline="\n", encoding="utf-8") as file:
+        async with aiofiles.open(file=path, mode="rt", encoding="utf-8") as file:
             soup = BeautifulSoup(await file.read(), "html.parser")
-            for link in soup.find_all("a"):
-                
+            for tag in soup.find_all("a", href=True):
                 async with asyncio.TaskGroup() as tg:
-                    tg.create_task(coro=self.management(url_root, dir_path, url_start, link))
+                    tg.create_task(coro=self.rip(tag, dir_path))
 
+    async def rip(self, tag, dir_path):
+        blacklist = ["https:", "www.gitbook.com", "https://www.gitbook.com/?utm_source=content&utm_medium=trademark&utm_campaign=SoR8NHCZ4ZjclRjRssSc", "?utm_source=content&utm_medium=trademark&utm_campaign=SoR8NHCZ4ZjclRjRssSc"]
+        link = tag["href"]
+        
+        for part in link.split("/"):
+            if "tt" in part:
+                print("n")
+            if part not in blacklist and link.split("/")[-2] != "computer-science-data-base":
+                os.makedirs(f"{dir_path}/{link}", exist_ok=True)
+                print(part)
 
-    async def management(self, url_root : str, dir_path : str, url_start : str, link : str):
-        not_allowed = ["https:", "www.gitbook.com", "groupeinfo.gitbook.io", "", "?utm_source=content&utm_medium=trademark&utm_campaign=SoR8NHCZ4ZjclRjRssSc"]
-        l = link.get("href")
-
-        for directory in l.split("/"):
-            if directory != l.split("/")[-1] and directory not in not_allowed or directory == "computer-science-data-base":
-                #print(directory)
-                dir_path = f"{dir_path}/{directory}"
-                os.makedirs(dir_path, exist_ok=True)
-
-        if url_root in l:
-            if l == "https://groupeinfo.gitbook.io/computer-science-data-base":
-                new_url = l
-
-            else:
-                new_url = ".".join(f"{url_start}{l}".split(".")[:-1])
-            
-            dreturn = f"{dir_path}/{await self.download(dir_path, new_url)}"
-            
-            async with aiofiles.open(file=dreturn, mode="rt", encoding="utf-8") as file:
-                soup = BeautifulSoup(await file.read(), "html.parser")
-                for link in soup.find_all("a", href=True):
-                    
-                    #if "#" in link["href"]:
-                    #    #print(link["href"].split("#"))
-                    #    if link["href"].split("#")[0] != "":
-                    #        link["href"] = new_url + ".html#" + link["href"].split("#")[-1]
-                    #        
-                    #    print(link["href"])
-                    #else:
-                    #    link["href"] = link["href"] + ".html"
-                        
-                    #for script in soup.find_all("script"):
-                    #    try:
-                    #        if script["src"] == "https://static.gitbook.com/_next/static/chunks/5681-732789c29ab58e75.js":
-                    #            script["src"] = ""
-                    #    
-                    #    except KeyError:
-                    #        continue
-                    
-                    async with aiofiles.open(file=dreturn, mode="wt", encoding="utf-8") as filer:
-                        await filer.write(str(soup.prettify()))
-                        
-                
-
-
-            
-            return dreturn
-
-
+        
+        
+        
+        
+        
 
 
 
 if __name__ == "__main__":
     
-    base_url = "https://gitbook.io"
-    url_start = "https://groupeinfo.gitbook.io"
-    dir_path = ".."
+    dir_path = "../computer-science-data-base"
+    file_path = "/computer-science-data-base"
     url_root = "/computer-science-data-base"
-    url = "".join((url_start, url_root))
+    url = "https://groupeinfo.gitbook.io/computer-science-data-base"
     
-    links = {"href" : url}
+    
+    
+    os.makedirs(dir_path, exist_ok=True)
     
     time = Time()
     
     #asyncio.run(main=time.rip(dir_path, url))
     
     link = Link()
-    asyncio.run(main=link.rip(dir_path=dir_path, url=url, url_root=url_root, url_start=url_start, link=links))
+    asyncio.run(main=link.management(f"{dir_path}{file_path}.html", url, dir_path))
